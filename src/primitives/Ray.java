403b6d2536bf7d1453c59d08866e527110b7f071
Tzachi <sassoontzachi@gmail.com>
2025-04-24 11:18:52 +0300

    changed ray attributes to private final and removed unused code in PlaneTest

--- src/primitives/Ray.java
+++ src/primitives/Ray.java
@@ -11,14 +11,14 @@ public class Ray {
      * Represents the starting point (head) of the ray in 3-dimensional space.
      * This is the origin point from which the ray extends infinitely in its direction.
      */
-    private Point head;
+    private final Point head;
 
     /**
      * Represents the direction vector of the ray.
      * This vector specifies the direction in which the ray extends from its starting point.
      * The direction vector is normalized and cannot be a zero vector.
      */
-    private Vector direction;
+    final Vector direction;
 
     /**
      * Constructs a {@code Ray} object using a starting point (head) and a direction vector.
--- unittests/geometries/PlaneTest.java
+++ unittests/geometries/PlaneTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import primitives.Point;
 import primitives.Vector;
 
-import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
