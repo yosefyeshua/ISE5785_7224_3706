755f67076323480f887a1a21dc136eb9ba807a70
Tzachi <sassoontzachi@gmail.com>
2025-04-29 17:09:54 +0300

    Added missing Javadoc for Geometries

--- src/geometries/Geometries.java
+++ src/geometries/Geometries.java
@@ -7,19 +7,45 @@ import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
+/**
+ * A composite geometry class that represents a collection of {@link Intersectable} objects.
+ * <p>
+ * Provides functionality to add multiple geometries and to find all intersection points
+ * between a given {@link Ray} and the contained geometries.
+ */
 public class Geometries implements Intersectable {
     private final List<Intersectable> geometries = new LinkedList<>();
 
     public Geometries() {}
 
+    /**
+     * Constructs a collection containing the given geometries.
+     *
+     * @param geometries the geometries to add
+     */
     public Geometries(Intersectable... geometries) {
         add(geometries);
     }
 
+    /**
+     * Adds one or more geometries to the collection.
+     *
+     * @param geometries the geometries to add
+     */
     public void add(Intersectable... geometries) {
         this.geometries.addAll(Arrays.asList(geometries));
     }
 
+    /**
+     * Finds all intersection points between the given {@link Ray} and all geometries
+     * in the collection.
+     * <p>
+     * Iterates through all contained geometries and collects their intersection points
+     * with the ray.
+     *
+     * @param ray the ray to intersect with the geometries
+     * @return a list of all intersection points found, or {@code null} if none were found
+     */
     @Override
     public List<Point> findIntersections(Ray ray) {
         List<Point> intersections = null;
