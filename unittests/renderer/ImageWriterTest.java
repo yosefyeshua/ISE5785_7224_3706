edb177bff61acc8de2ab34ecb1dfe5ecca84fb2e
yosefyeshua <yosefyeshua4@gmail.com>
2025-05-20 19:50:18 +0300

    test

--- unittests/renderer/ImageWriterTest.java
+++ unittests/renderer/ImageWriterTest.java
@@ -23,4 +23,30 @@ class ImageWriterTest {
         }
         IW.writeToImage("test");
     }
+
+    @Test
+    void SECImageTest() {
+        ImageWriter IW = new ImageWriter(800, 500);
+        int k=0;
+        int l=0;
+        for (int i = 0; i < IW.nX(); ++i)
+        {
+            for (int j = 0; j < IW.nY(); ++j)
+            {
+                if (i % 50 >= 25 && j % 50>=25)
+                    IW.writePixel(i, j, new Color(255-i%255, 255- j%255, l));
+                else
+                    IW.writePixel(i, j, new Color(j%255, i%255, k));
+
+                k +=1;
+                l+=2;
+                if (k>=255)
+                    k=0;
+                if (l>=255)
+                    l=0;
+            }
+        }
+        IW.writeToImage("test22");
+    }
+
 }
