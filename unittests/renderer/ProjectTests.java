9c7cf443df5b4c3da0326d752832bb0ad64baf14
Tzachi <sassoontzachi@gmail.com>
2025-07-27 16:02:28 +0300

    project test push

--- unittests/renderer/ProjectTests.java
+++ unittests/renderer/ProjectTests.java
@@ -127,15 +127,16 @@ public class ProjectTests {
                 .setVpDistance(150)
                 .setVpSize(200, 200)
                 .setResolution(1000, 1000)
-                .setAaSamples(10)
+                //.setAaSamples(10)
                 //.setASSdepth(5)
-                .setMultithreading(8)
+                //.setMultithreading(8)
                 .setDebugPrint(0.1)
                 //.setDofSamples(81)
                 .setFocalDistance(new Point(0, 0, -50).distance(new Point(0, 20, 70))) // Focal distance to the main sphere
                 .setApertureRadius(2.0)
+                .setASSdepthDOF(2)
                 .build();
 
-        camera.renderImage().writeToImage("FinalProject2Image_MTTEST");
+        camera.renderImage().writeToImage("FinalProject2Image_AAASS_TEMP");
     }
 }
